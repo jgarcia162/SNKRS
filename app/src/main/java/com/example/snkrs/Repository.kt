4c22fec17e419b73d4ecb606f87a84716211972e
joseog-geniusplaza <jose@geniusplaza.com>
2021-06-05 06:55:24 -0400

    implement hilt di

--- app/src/main/java/com/example/snkrs/Repository.kt
+++ app/src/main/java/com/example/snkrs/Repository.kt
@@ -1,7 +1,8 @@
 package com.example.snkrs
 
 import com.example.snkrs.network.ApiClient
+import javax.inject.Inject
 
-class Repository(private val apiClient: ApiClient) {
+class Repository @Inject constructor(private val apiClient: ApiClient) {
   fun getTopRated() = apiClient.getTopRated();
 }
