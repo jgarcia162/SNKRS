66b3269bc54c62b5df441ff1a95df5e85a480e87
joseog-geniusplaza <jose@geniusplaza.com>
2021-06-07 23:31:22 -0400

    add upcoming movies rv

--- app/src/main/java/com/example/snkrs/MainActivity.kt
+++ app/src/main/java/com/example/snkrs/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.snkrs.adapter.CarouselLayoutManager
 import com.example.snkrs.adapter.MovieAdapter
 import com.example.snkrs.extensions.toast
+import com.example.snkrs.model.Movie
 import com.example.snkrs.viewmodel.MoviesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,25 +17,34 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   private val moviesViewModel: MoviesViewModel by viewModels()
-  private lateinit var movieAdapter: MovieAdapter
+  private lateinit var topRatedAdapter: MovieAdapter
+  private lateinit var upcomingAdapter: MovieAdapter
   private lateinit var topRatedRV: RecyclerView
+  private lateinit var upcomingRV: RecyclerView
   @Inject lateinit var repository: Repository
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     topRatedRV = findViewById(R.id.top_rated_movies_rv)
+    upcomingRV = findViewById(R.id.upcoming_movies_rv)
   
     initRV()
   }
   
   private fun initRV() {
     topRatedRV.layoutManager = CarouselLayoutManager(this)
-    movieAdapter = MovieAdapter(emptyList()) { movie ->
+    upcomingRV.layoutManager = CarouselLayoutManager(this)
+    val onMovieClicked: (Movie) -> Unit = { movie ->
       Toast.makeText(applicationContext, movie.title, Toast.LENGTH_SHORT).show()
     }
-    topRatedRV.adapter = movieAdapter
+    topRatedAdapter = MovieAdapter(emptyList(), onMovieClicked)
+    topRatedRV.adapter = topRatedAdapter
     PagerSnapHelper().attachToRecyclerView(topRatedRV)
+    
+    upcomingAdapter = MovieAdapter(emptyList(), onMovieClicked)
+    upcomingRV.adapter = upcomingAdapter
+    PagerSnapHelper().attachToRecyclerView(upcomingRV)
   }
   
   override fun onStart() {
@@ -46,20 +56,20 @@ class MainActivity : AppCompatActivity() {
       toast("couldnt load movies")
     })
     
-//    moviesViewModel.getUpcomingMovies({
-//      toast("movies loaded")
-//    }, {
-//      toast("couldnt load movies")
-//    })
+    moviesViewModel.getUpcomingMovies({
+      toast("movies loaded")
+    }, {
+      toast("couldnt load movies")
+    })
   }
   
   private fun observeLiveData(){
     moviesViewModel.topRatedMoviesLiveData.observe(this, {movies ->
-      movieAdapter.setData(movies)
+      topRatedAdapter.setData(movies)
     })
-//    moviesViewModel.upcomingMoviesLiveData.observe(this, {movies ->
-//      movieAdapter.setData(movies)
-//    })
+    moviesViewModel.upcomingMoviesLiveData.observe(this, {movies ->
+      upcomingAdapter.setData(movies)
+    })
   }
 }
 
